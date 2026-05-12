efe0b70c8d86e8bd763504e84e0b63c22802162b
Anatole DP <anatoli.dp@gmail.com>
2026-05-12 03:20:01 -0500

    remove godot things

--- godot/modules/mono/editor/GodotTools/GodotTools.IdeMessaging/IMessageHandler.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace GodotTools.IdeMessaging
-{
-    public interface IMessageHandler
-    {
-        public Task<MessageContent> HandleRequest(Peer peer, string id, MessageContent content, ILogger logger);
-    }
-}
